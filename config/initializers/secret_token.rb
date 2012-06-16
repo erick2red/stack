587b809978f1c1bcfdb514518de5e2effcff2af9
Erick Pérez Castellanos <erick.red@gmail.com>
2012-06-16 00:21:45 -0400

    Initial Commit. Stock configuration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stack::Application.config.secret_token = 'b87f3aaac7b3f25c73af5a95ce62631f962bfff374de74bdd1b78c4233e44f686d164ee15c8b9bb18fb95e956930d52db1016265f58637541536ce3a74ff8d6a'
