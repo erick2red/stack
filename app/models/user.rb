8f88d0f17f141b4cbfbaef11a7ef1e64d983b7e2
Tulkas Eldarion <tulkas@tulkas.datys.cu>
2012-06-21 09:46:51 -0400

    Merging with HEAD.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true, :uniqueness => true
   validates :email, :presence => true, :uniqueness => true
-  validates :password, :presence => true
+  validates :password, :presence => true,
+                       :length => { :minimum => 6 },
+                       :allow_nil => false,
+                       :allow_blank => false
 
   class << self
     def authenticate_by_email(email, password)
