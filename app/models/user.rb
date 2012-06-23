39ace81a6705213ea993b262ee2d086c85807aba
Erick Pérez Castellanos <erick.red@gmail.com>
2012-06-23 00:55:25 -0400

    Improving password saving in db.
    Removed plain password. Using bcrypt-ruby for hashing password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :username, :language
+  attr_accessible :email, :name, :username, :language
+  has_secure_password
 
   validates :username, :presence => true, :uniqueness => true
-  validates :email, :presence => true, :uniqueness => true
+  validates :email, :presence => true,
+                    :uniqueness => true,
+                    :format => {
+                      :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/,
+                      :message => I18n.t(:malformed_email)
+                    }
+
   validates :password, :presence => true,
                        :length => { :minimum => 6 },
                        :allow_nil => false,
@@ -11,19 +18,14 @@ class User < ActiveRecord::Base
   has_many :protected_actions
 
   class << self
-    def authenticate_by_email(email, password)
-      if user = find_by_email(email)
-        if user.password == password
-          user
-        end
+    def authenticate(name, password)
+      if name.include?('@')
+        user = User.find_by_email(name)
+      else
+        user = User.find_by_username(name)
       end
-    end
-
-    def authenticate_by_username(username, password)
-      if user = find_by_username(username)
-        if user.password == password
-          user
-        end
+      if user and user.authenticate(password)
+        user
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623025248) do
+ActiveRecord::Schema.define(:version => 20120623031202) do
 
   create_table "protected_actions", :force => true do |t|
     t.string   "token"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(:version => 20120623025248) do
     t.string   "name"
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "enabled",    :default => false
-    t.string   "language",   :default => "en"
+    t.string   "password_digest"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.boolean  "enabled",         :default => false
+    t.string   "language",        :default => "en"
   end
 
 end
